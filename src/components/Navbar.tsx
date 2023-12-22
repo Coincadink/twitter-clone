fb2731eb3268dc11a313b3880c4120261cfe7dc3
John Peterson <johnp5419@gmail.com>
2023-12-21 18:49:31 -0700

    Cleaning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,11 +33,26 @@ const NAV_ITEMS = [
 ]
 
 const Navbar = () => {
-  return (
-    <section className='w-0 invisible sm:visible sm:w-[80px] xl:w-[340px] flex justify-center items-center'>
-        LEFT NAVBAR
-    </section>
-  )
+	return (
+		<section className='w-0 invisible sm:visible sm:w-[80px] xl:w-[340px] flex flex-col justify-between items-center'>
+			<div className="w-full p-2">
+				<Link href={"/"} className='my-4 p2'>
+					<BsTwitter className='relative left-2 text-4xl'/>
+				</Link>
+				{
+					NAV_ITEMS.map((item)=>(
+						<Link className = 'hover:bg-white/10 transition duration-200 text-2xl rounded-3xl p-3 flex items-center justify-start w-fit space-x-4' href={`/${item.title.toLowerCase()}`} key = {item.title}>
+							<div><item.icon/></div>
+							<div>{item.title}</div>
+						</Link>
+					))
+				}
+				<Link className='w-full h-20 bg-blue-500 flex items-center justify-center p-100 rounded-4xl' href="#">POST</Link>
+			</div>
+
+			<div>PROFILE SECTION</div>
+		</section>
+	)
 }
 
 export default Navbar
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,6 @@
 const Timeline = () => {
   return (
-    <main className='w-[600px] flex justify-center items-center'>
+    <main className='w-[600px] flex justify-center items-center border-solid border-[0.01em] border-gray-300 border-opacity-25'>
         HOME TIMELINE
     </main>
   )
